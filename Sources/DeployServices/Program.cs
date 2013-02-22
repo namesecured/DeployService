77106b779912f47450a6bfd9e26f45ef092a5afe
Sergey Psekha <pss@ciklum.com>
2013-02-22 13:19:03 +0200

    batching 6

---
+++ Sources/DeployServices/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployServices
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ok");
+            string TestString = "This is a <Test String>.";
+            string EncodedString = System.Web.HttpUtility.UrlDecode("Text=\"%40(Server >'%25(Name)'):");
+        }
+    }
+}
